6026cdd6f9fb9f1c55f1a160655bc3bcff20aa23
Kori <kexin@korix.top>
2022-11-27 23:42:36 +0800

    css 模块化

--- react_staging/src/components/Welcome/index.jsx
+++ react_staging/src/components/Welcome/index.jsx
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import './index.css'
+import css from './index.module.css'
 
 export default  class Welcome extends Component {
   render () {
     return (
       <div>
-        <h1 className='welcome'>React</h1>
+        <h1 className={css.welcome}>React</h1>
       </div>
     )
   }
