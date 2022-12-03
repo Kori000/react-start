c30117e037a358d587c576095e6ecc050918fb45
Kori <kexin@korix.top>
2022-12-03 13:19:53 +0800

    antd

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Link, Route, NavLink, Switch, Redirect, HashRouter } from 'react-router-dom';
-import About from './components/About';
-import About2 from './components/News';
-import Home from './components/Home';
-import MyNavLink from './components/MyNavLink';
+import { Button } from 'antd';
+
+
+
 
 
 export default class App extends Component {
@@ -16,7 +15,6 @@ export default class App extends Component {
 
 
   updateState = (update) => {
-
     this.setState(update)
   }
 
@@ -25,31 +23,11 @@ export default class App extends Component {
 
     return (
       <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-xs-8">
-            <div className="page-header"><h2>React Router Demo</h2></div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              <MyNavLink to="/about">About</MyNavLink>
-              <MyNavLink to="/home">Home</MyNavLink>
-              {/* <a className="list-group-item" href="/about">About</a>
-              <a className="list-group-item active" href="/home" aria-current="page">Home</a> */}
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                <div>
-                  <Route path="/home" component={Home} />
-                  <Route path="/about" component={About} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
       </div>
     )
   }
--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -3,13 +3,9 @@ import React from 'react'
 // DOM 库
 import ReactDOM from 'react-dom/client'
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-
+  <App />
 )
