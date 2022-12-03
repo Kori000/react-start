b15c8846f3d91ef1b92a1b326a9f924e5f81fb11
Kori <kexin@korix.top>
2022-12-03 23:37:10 +0800

    引入 redux-devtools-extension

--- react_staging/src/redux/store.js
+++ react_staging/src/redux/store.js
@@ -4,12 +4,14 @@ import { combineReducers, legacy_createStore as createStore } from 'redux';
 import countReducer from './reducers/count';
 import personReducer from './reducers/person';
 
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 const allRedecers = combineReducers({
   count: countReducer,
   person: personReducer
 })
 
 
-export default createStore(allRedecers)
+export default createStore(allRedecers, composeWithDevTools())
 
 
